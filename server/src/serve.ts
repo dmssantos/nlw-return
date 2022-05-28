98853f329d02704c464a3725fbae48aa499f88c4
dmssantos <dms.santos_@hotmail.com>
2022-05-28 10:07:19 -0300

    First commit server config

---
+++ server/src/serve.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/users', (req, res) => {
+  return res.send('Hello world')
+})
+
+app.listen(3333, () => {
+  console.log('HTTP server runing!');
+})
