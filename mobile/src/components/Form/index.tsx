49c7b214e23bed31c992a63cdd8066ca385e5086
dmssantos <dms.santos_@hotmail.com>
2022-06-06 15:38:47 -0300

    screenshot

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -9,6 +9,8 @@ import {
 import { ArrowLeft } from 'phosphor-react-native';
 
 import { FeedbackType } from '../../components/Widget';
+import { ScreenshotButton } from '../ScreenshotButton';
+import { Button } from '../../components/Button';
 
 import { theme } from '../../theme';
 import { styles } from './styles';
@@ -51,6 +53,16 @@ export function Form({ feedbackType }: Props) {
         placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo"
         placeholderTextColor={theme.colors.text_secondary}
       />
+
+      <View style={styles.footer}>
+        <ScreenshotButton
+          onTakeShot={() => { }}
+          onRemoveShot={() => { }}
+          screenshot=""
+        />
+
+        <Button isLoading={false} />
+      </View>
     </View>
   );
 }
